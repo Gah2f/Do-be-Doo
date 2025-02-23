58c5766b52dd5c79cb097aa13a61bd20db059a3e
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2025-02-23 17:24:15 +0300

    Filtering the tasks: default, completed and incompleted

---
+++ src/components/Filter.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { filterTodo, markAllCompleted } from '../redux/actions';
+
+function Filter() {
+    const dispatch = useDispatch();
+    const currentFilter = useSelector((state) => state.filter);
+    const handleFilterChange = (filter) => {
+        dispatch(filterTodo(filter));
+    }
+  return (
+    <div className='flex space-x-4 items-center'>
+        <select id='selectFilter' name='selectFilter'
+         className='text-sm px-2 py-1 rounded border border-gray-300 focus:outline-none' 
+        value={currentFilter}
+        onChange={(e)=> handleFilterChange(e.target.value)}>
+            <option value="ALL">
+                Default
+            </option>
+            <option value="COMPLETED">
+                Completed
+            </option>
+            <option value="INCOMPLETED">
+                Incompleted
+            </option>
+        </select>
+        <button onClick={()=> dispatch(markAllCompleted())} 
+        className='text-sm px-2 py-1 bg-purple-500 text-white ml-2 rounded'>
+            Mark All Completed
+        </button>
+    </div>
+  )
+}
+
+export default Filter
